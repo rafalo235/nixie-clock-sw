e37f2ed6ffd12d6237bb9a9bc0aea22a749995da
Rafał Olejniczak <rafalo235@gmail.com>
2017-08-14 18:30:41 +0200

    control: fixes encoder handling - fixes encoder handling

--- src/drivers/gpio/isr.c
+++ src/drivers/gpio/isr.c
@@ -9,19 +9,69 @@
 #include "control-task/control-task.h"
 #include "stm32f103xb.h"
 
+#define IS_RISING_EDGE(x) \
+	((GPIOA->IDR & GPIO_IDR_IDR##x) == GPIO_IDR_IDR##x)
+#define IS_FALLING_EDGE(x) \
+	((GPIOA->IDR & GPIO_IDR_IDR##x) != GPIO_IDR_IDR##x)
+#define IS_HIGH(x) \
+	((GPIOA->IDR & GPIO_IDR_IDR##x) == GPIO_IDR_IDR##x)
+#define IS_LOW(x) \
+	((GPIOA->IDR & GPIO_IDR_IDR##x) != GPIO_IDR_IDR##x)
+
+enum QuadratureState {
+	ENCODER_IDLE,
+	ENCODER_PENDING_DECREMENT,
+	ENCODER_PENDING_INCREMENT
+};
+
 void EXTI1_IRQHandler(void)
 {
-	const tControlAction action = CONTROL_ACTION_DECREMENT;
-	BaseType_t higherTaskPriority = pdFALSE;
+	static enum QuadratureState sState = ENCODER_IDLE;
 
 	EXTI->PR = EXTI_PR_PR1;
 
-	if ((GPIOA->IDR & GPIO_IDR_IDR2) == GPIO_IDR_IDR2 &&
-			pdFALSE == xQueueIsQueueFullFromISR(gControlQueue))
+	if (ENCODER_IDLE == sState)
 	{
-		xQueueSendToBackFromISR(
-				gControlQueue, &action, &higherTaskPriority);
-		portYIELD_FROM_ISR(higherTaskPriority);
+		if (IS_FALLING_EDGE(1) && IS_HIGH(2))
+		{
+			sState = ENCODER_PENDING_DECREMENT;
+		}
+		else if (IS_FALLING_EDGE(1) && IS_LOW(2))
+		{
+			sState = ENCODER_PENDING_INCREMENT;
+		}
+	}
+	else if (ENCODER_PENDING_DECREMENT == sState
+			&& IS_RISING_EDGE(1) && IS_LOW(2))
+	{
+		tControlAction action = CONTROL_ACTION_DECREMENT;
+		sState = ENCODER_IDLE;
+
+		if (pdFALSE == xQueueIsQueueFullFromISR(gControlQueue))
+		{
+			BaseType_t higherTaskPriority = pdFALSE;
+			xQueueSendToBackFromISR(
+					gControlQueue, &action, &higherTaskPriority);
+			portYIELD_FROM_ISR(higherTaskPriority);
+		}
+	}
+	else if (ENCODER_PENDING_INCREMENT == sState
+			&& IS_RISING_EDGE(1) && IS_HIGH(2))
+	{
+		tControlAction action = CONTROL_ACTION_INCREMENT;
+		sState = ENCODER_IDLE;
+
+		if (pdFALSE == xQueueIsQueueFullFromISR(gControlQueue))
+		{
+			BaseType_t higherTaskPriority = pdFALSE;
+			xQueueSendToBackFromISR(
+					gControlQueue, &action, &higherTaskPriority);
+			portYIELD_FROM_ISR(higherTaskPriority);
+		}
+	}
+	else
+	{
+		sState = ENCODER_IDLE;
 	}
 }
 
