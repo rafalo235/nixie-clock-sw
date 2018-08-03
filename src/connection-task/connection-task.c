a086af7d9b53a269e1fcd6ecb968a461dcf4b11a
Rafał Olejniczak <rafalo235@gmail.com>
2018-08-03 16:02:11 +0200

    connection-task: refactoring, loop change; - split connection-task loop to subtasks; - reruning server on accept fail;

--- src/connection-task/connection-task.c
+++ src/connection-task/connection-task.c
@@ -24,13 +24,25 @@
 #include "control-task/control-task.h"
 #include "connection-task/configuration.h"
 
-static espr_t
-Connection_Callback(esp_evt_t* evt);
+typedef enum ConnectionResult
+{
+  CONNECTION_RESULT_OK,
+  CONNECTION_SERVER_BROKEN,
+  CONNECTION_SERVER_OUT_OF_RESOURCES,
+  CONNECTION_SERVER_BIND_ERROR,
+  CONNECTION_SERVER_LISTEN_ERROR
+} tConnectionResult;
+
+static espr_t Connection_Callback(esp_evt_t* evt);
+
+static tConnectionResult Connection_RunServer(void);
+
+static void Connection_HandleClient(esp_netconn_p client);
 
 void Connection_Task(void *parameters)
 {
   espr_t res;
-  esp_netconn_p server, client;
+  tConnectionResult connResult;
 
   if ((res = esp_init(&Connection_Callback, 1)) == espOK) {
     asm volatile ("nop");
@@ -63,65 +75,111 @@ void Connection_Task(void *parameters)
     Connection_SetConnected(0);
   }
 
+  Routine_Init(&gConnectionRoutine);
+
+  /* ---- Start connection task loop ---- */
+  while (1)
+  {
+    connResult = Connection_RunServer();
+    if (CONNECTION_SERVER_BROKEN != connResult)
+    {
+      /* Wait for resources if trivial functions fails */
+      vTaskDelay(pdMS_TO_TICKS(100));
+    }
+  }
+  /* ---- Start connection task loop ---- */
+}
+
+static tConnectionResult Connection_RunServer(void)
+{
+  espr_t res;
+  tConnectionResult result;
+  esp_netconn_p server, client;
+
   server = esp_netconn_new(ESP_NETCONN_TYPE_TCP);
   if (NULL == server)
   {
-    configASSERT(0); /* fixme remove asserts */
+    result = CONNECTION_SERVER_OUT_OF_RESOURCES;
   }
-  res = esp_netconn_bind(server, 80);
-  if (res != espOK)
+  else
   {
-    configASSERT(0);
-  }
-  res = esp_netconn_listen(server);
-  if (res != espOK)
-  {
-    configASSERT(0);
-  }
-
-  Routine_Init(&gConnectionRoutine);
-
-  while (1)
-  {
-    res = esp_netconn_accept(server, &client);
-    if (res == espOK)
+    res = esp_netconn_bind(server, 80);
+    if (res != espOK)
     {
-      static tuCHttpServerState sServerState;
-      esp_pbuf_p pbuf, prev;
-
-      Http_InitializeConnection(
-          &sServerState, &Http_SendPort, &OnError,
-          &resources, 11, client);
-
-      esp_netconn_set_receive_timeout(client, 500);
-
-      do
+      result = CONNECTION_SERVER_BIND_ERROR;
+    }
+    else
+    {
+      res = esp_netconn_listen(server);
+      if (res != espOK)
       {
-        res = esp_netconn_receive(client, &pbuf);
+        result = CONNECTION_SERVER_LISTEN_ERROR;
+      }
+      else
+      {
 
-        if (espOK == res)
+        /* ---- Start main server loop ---- */
+        while (1)
         {
-          while (NULL != pbuf)
+          res = esp_netconn_accept(server, &client);
+          if (res == espOK)
           {
-            Http_Input(&sServerState, esp_pbuf_data(pbuf), esp_pbuf_length(pbuf, 0u));
-            prev = pbuf;
-            pbuf = esp_pbuf_unchain(prev);
-            esp_pbuf_free(prev);
+            Connection_HandleClient(client);
           }
-          esp_netconn_flush(client);
+          else
+          {
+            /* Server netconn is gone */
+            esp_netconn_close(server);
+            esp_netconn_delete(server);
+            break;
+          }
         }
+        /* ---- End main server loop ---- */
+        result = CONNECTION_SERVER_BROKEN;
+      }
+    }
+  }
 
-      } while (espOK == res);
-      /* After this loop connection is closed */
+  return result;
+}
 
-      /* Make sure connection is closed ?todo is necessary */
-      esp_netconn_close(client);
-      esp_netconn_delete(client);
+static void Connection_HandleClient(esp_netconn_p client)
+{
+  espr_t res;
+  static tuCHttpServerState sServerState;
+  esp_pbuf_p pbuf, prev;
 
-      Routine_ExecuteRoutine(&gConnectionRoutine);
+  Http_InitializeConnection(&sServerState, &Http_SendPort,
+      &OnError, &resources, 11, client);
+
+  esp_netconn_set_receive_timeout(client, 500);
+
+  do
+  {
+    res = esp_netconn_receive(client, &pbuf);
+
+    if (espOK == res)
+    {
+      while (NULL != pbuf)
+      {
+        Http_Input(&sServerState, esp_pbuf_data(pbuf),
+            esp_pbuf_length(pbuf, 0u));
+        prev = pbuf;
+        pbuf = esp_pbuf_unchain(prev);
+        esp_pbuf_free(prev);
+      }
+      esp_netconn_flush(client);
     }
 
-  }
+  } while (espOK == res);
+  /* After this loop connection is closed */
+
+  /* Make sure connection is closed ?todo is necessary */
+  esp_netconn_close(client);
+  esp_netconn_delete(client);
+
+  Routine_ExecuteRoutine(&gConnectionRoutine);
+
 }
 
 static espr_t Connection_Callback(esp_evt_t* evt)
